92f49732a7bda0ada3beb94d20362b44481a15ef
Mihail Karetnikov <mihail.karetnikov77712@gmail.com>
2023-10-26 10:43:35 +0300

    working...

--- js/friends.js
+++ js/friends.js
@@ -1,26 +1,25 @@
-const body = document.body
+const wrapper = document.querySelector('.wrapper')
 const friends = document.querySelector('.friends')
 const closingX = friends.querySelector('.closing-x')
 const openingButton = document.querySelector('.header__friends')
+const headerProfile = document.querySelector('.header__profile')
 
 closingX.addEventListener('click', friendsClose)
 openingButton.addEventListener('click', friendsOpen)
 
-function frindsButtonShift() {
-	openingButton.classList.toggle('header__friends--closed')
-	document.querySelector('.header__profile').classList.toggle('.header__profile--closed')
-}
 function friendsClose() {
-	body.style.gridTemplateColumns = '310px 1fr 0px'
-	friends.classList.toggle('closed')
-	console.log('closed')
+	wrapper.classList.toggle('wrapper--closed')
+	friends.classList.toggle('friends--closed')
 	frindsButtonShift()
 }
 function friendsOpen() {
-	if (friends.classList.contains('closed')) {
-		body.style.gridTemplateColumns = '310px 1fr 310px'
+	if (friends.classList.contains('friends--closed')) {
+		wrapper.classList.toggle('wrapper--closed')
+		friends.classList.toggle('friends--closed')
+		frindsButtonShift()
 	}
-	friends.classList.toggle('closed')
-	console.log('opened')
-	frindsButtonShift()
+}
+function frindsButtonShift() {
+	openingButton.classList.toggle('header__friends--opened')
+	headerProfile.classList.toggle('header__profile--opened')
 }
