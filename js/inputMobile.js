74df8e561028e9790240f98e06e35a87593eca5e
Mihail Karetnikov <mihail.karetnikov77712@gmail.com>
2023-10-26 19:15:25 +0300

    working...

---
+++ js/inputMobile.js
@@ -0,0 +1,31 @@
+// const inputMobile = document.querySelector('.input__mobile')
+// const searchMobile = document.querySelector('.header__search')
+
+// searchMobile.addEventListener('click', showInput)
+// document.addEventListener('click', hideInput)
+
+// function showInput(event) {
+// 	console.log('clientWidth: ', document.documentElement.clientWidth)
+// 	if (document.documentElement.clientWidth <= 1000) {
+// 		inputMobile.classList.add('visible')
+// 	}
+// }
+// function hideInput(event) {
+// 	if (searchMobile != event.target
+// 		&&
+// 		!searchMobile.contains(event.target)
+// 		&&
+// 		inputMobile.classList.contains('visible')
+// 		)
+// 	{
+// 		inputMobile.classList.remove('visible')
+// 	}
+// }
+// window.addEventListener('resize', () => {
+// 	if (document.documentElement.clientWidth > 1000) {
+// 		inputMobile.classList.remove('visible')
+// 	}
+// })
+// document.addEventListener('click', (event) => {
+// 	console.log(event.target)
+// })
